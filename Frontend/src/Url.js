effe6a543dc56bdb5af1f3e745c679c6dcf409e5
jaykit <jaykitmaurya19@gmail.com>
2025-07-25 19:30:45 +0530

    updated url.js file

--- Frontend/src/Url.js
+++ Frontend/src/Url.js
@@ -1,6 +1,6 @@
 //export const BASE_URL = "http://localhost:5000";
 
-export const BASE_URL="https://chat-app-backend-seven-xi.vercel.app";
+export const BASE_URL="https://chat-app-backend-lake-one.vercel.app";
 
 export const LOGIN_URL = `${BASE_URL}/api/auth/login`;
 export const SIGNUP_URL = `${BASE_URL}/api/auth/signup`;
