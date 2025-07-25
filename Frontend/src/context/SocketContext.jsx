db881d77bc75b7fa21c85c8bd4d9ae80a6ea0192
jaykit <jaykitmaurya19@gmail.com>
2025-07-25 20:19:53 +0530

    updated link

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chatify-f36x.onrender.com", {
+			const socket = io("https://chat-app-backend-lake-one.vercel.app", {
 				query: {
 					userId: authUser._id,
 				},
