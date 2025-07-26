bd636b764bf23d29939e8028b97a10c2c946450c
jaykit <jaykitmaurya19@gmail.com>
2025-07-26 18:50:35 +0530

    updated frontend

--- Frontend/src/components/messages/Messages.jsx
+++ Frontend/src/components/messages/Messages.jsx
@@ -18,12 +18,14 @@ const Messages = () => {
 	return (
 		<div className='px-4 flex-1 overflow-auto'>
 			{!loading &&
+				Array.isArray(messages) &&
 				messages.length > 0 &&
 				messages.map((message) => (
-					<div key={message._id} ref={lastMessageRef}>
-						<Message message={message} />
-					</div>
-				))}
+				<div key={message._id} ref={lastMessageRef}>
+					<Message message={message} />
+		</div>
+	))}
+
 
 			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 			{!loading && messages.length === 0 && (
