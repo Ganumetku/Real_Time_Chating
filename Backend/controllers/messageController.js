8dceb5d6fb565b2dfecbdb2e5d2f765b5e6f74b5
jaykit <jaykitmaurya19@gmail.com>
2025-07-26 20:41:19 +0530

    updated final work

--- Backend/controllers/messageController.js
+++ Backend/controllers/messageController.js
@@ -69,7 +69,7 @@ export const getMessages = async (req, res) => {
         if (!conversation) return res.status(200).json([]);
 
         const messages = conversation.messages;
-        console.log(messages);
+        
 
         res.status(200).json(messages);
     } catch (error) {
